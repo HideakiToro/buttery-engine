bc2b57fed60fdb99edfff6560278e3800554dbb7
hideaki <hideaki.toro@gmail.com>
2026-05-01 18:23:22 +0200

    example: More UI-Elements & v0.6.0

--- example/src/core.rs
+++ example/src/core.rs
@@ -9,7 +9,8 @@ use buttery_engine::{
     key_event::{Key, KeyEvent},
     object::Object,
     ui::{
-        ButterUIWindowOffset, ButteryUIButton, ButteryUIColor, ButteryUIElement, ButteryUIModel,
+        ButterUI2D, ButteryUIButton, ButteryUIColor, ButteryUIContainer, ButteryUIContainerOutline,
+        ButteryUIDirectional, ButteryUIElement, ButteryUIInput, ButteryUIModel, ButteryUIText,
         ButteryUIWindow, ButteryUIWindowRelativePosition,
     },
 };
@@ -44,28 +45,164 @@ impl ButteryExample {
         }
     }
 
-    fn build_ui_model(&mut self) -> ButteryUIModel<ButteryExample> {
-        ButteryUIModel {
+    fn build_hud_model(&mut self) -> ButteryUIModel<ButteryExample> {
+        let mut model = ButteryUIModel {
             windows: vec![ButteryUIWindow {
-                max_width: 600.0,
-                max_height: 400.0,
-                corner_radius: 10.0,
-                inner_margin: 16,
-                child: ButteryUIElement::Column(vec![
-                    ButteryUIElement::Text(self.fps_text.clone()),
-                    ButteryUIElement::Text(self.secondary_text.clone()),
-                    ButteryUIElement::Button(ButteryUIButton {
-                        label: "Test".into(),
-                        on_click: |game| {
-                            game.secondary_text = "Test clicked".into();
-                        },
-                    }),
-                ]),
-                background_color: ButteryUIColor::default(),
-                offset: ButterUIWindowOffset::default(),
-                relative_position: ButteryUIWindowRelativePosition::default(),
+                id: "fps_counter".into(),
+                max_width: 100.0,
+                max_height: 40.0,
+                child: ButteryUIElement::Column(ButteryUIDirectional {
+                    children: vec![ButteryUIElement::Text(ButteryUIText {
+                        text: self.fps_text.clone(),
+                        ..Default::default()
+                    })],
+                    centered: true,
+                    size: None,
+                }),
+                relative_position: ButteryUIWindowRelativePosition::TopRight,
+                offset: ButterUI2D { x: -20.0, y: 20.0 },
+                ..Default::default()
             }],
+        };
+
+        if !self.open_menu {
+            model.windows.push(ButteryUIWindow {
+                id: "open_menu_btn".into(),
+                max_width: 100.0,
+                max_height: 40.0,
+                child: ButteryUIElement::Column(ButteryUIDirectional {
+                    children: vec![ButteryUIElement::Button(ButteryUIButton {
+                        label: "Open Menu".into(),
+                        on_click: |game: &mut ButteryExample| {
+                            game.open_menu = true;
+                        },
+                        ..Default::default()
+                    })],
+                    centered: true,
+                    size: None,
+                }),
+                relative_position: ButteryUIWindowRelativePosition::TopLeft,
+                background_color: ButteryUIColor {
+                    r: 0,
+                    g: 0,
+                    b: 0,
+                    a: 0,
+                },
+                padding: 0,
+                offset: ButterUI2D { x: 20.0, y: 20.0 },
+                ..Default::default()
+            });
         }
+
+        model
+    }
+
+    fn build_ui_model(&mut self) -> ButteryUIModel<ButteryExample> {
+        let mut model = self.build_hud_model();
+
+        model.windows.push(ButteryUIWindow {
+            id: "menu".into(),
+            max_width: 250.0,
+            max_height: 400.0,
+            child: ButteryUIElement::Row(ButteryUIDirectional {
+                children: vec![
+                    ButteryUIElement::Column(ButteryUIDirectional {
+                        children: vec![
+                            ButteryUIElement::Button(ButteryUIButton {
+                                label: "Test".into(),
+                                on_click: |game| {
+                                    game.secondary_text = "Test clicked".into();
+                                },
+                                ..Default::default()
+                            }),
+                            ButteryUIElement::Container(ButteryUIContainer {
+                                children: vec![
+                                    ButteryUIElement::Button(ButteryUIButton {
+                                        label: "Test".into(),
+                                        ..Default::default()
+                                    }),
+                                    ButteryUIElement::Button(ButteryUIButton {
+                                        label: "Test".into(),
+                                        ..Default::default()
+                                    }),
+                                ],
+                                color: ButteryUIColor {
+                                    r: 255,
+                                    g: 0,
+                                    b: 0,
+                                    a: 255,
+                                },
+                                corner_radius: 20.0,
+                                size: Some(ButterUI2D { x: 10.0, y: 30.0 }),
+                                outline: Some(ButteryUIContainerOutline {
+                                    color: ButteryUIColor {
+                                        a: 255,
+                                        r: 255,
+                                        g: 255,
+                                        b: 255,
+                                    },
+                                    width: 1.0,
+                                }),
+                            }),
+                            ButteryUIElement::Button(ButteryUIButton {
+                                label: "Close Menu".into(),
+                                on_click: |game| {
+                                    game.open_menu = false;
+                                },
+                                ..Default::default()
+                            }),
+                        ],
+                        centered: false,
+                        size: None,
+                    }),
+                    ButteryUIElement::Column(ButteryUIDirectional {
+                        children: vec![
+                            ButteryUIElement::Button(ButteryUIButton {
+                                label: "Test".into(),
+                                on_click: |game| {
+                                    game.secondary_text = "Test clicked".into();
+                                },
+                                ..Default::default()
+                            }),
+                            ButteryUIElement::Input(ButteryUIInput {
+                                current_value: self.secondary_text.clone(),
+                                on_changed: |value, game| {
+                                    game.secondary_text = value;
+                                },
+                                size: Some(ButterUI2D { x: 200.0, y: 50.0 }),
+                                background_color: Some(ButteryUIColor {
+                                    r: 255,
+                                    g: 0,
+                                    b: 0,
+                                    a: 255,
+                                }),
+                            }),
+                            ButteryUIElement::Input(ButteryUIInput {
+                                current_value: self.secondary_text.clone(),
+                                on_changed: |value, game| {
+                                    game.secondary_text = value;
+                                },
+                                ..Default::default()
+                            }),
+                            ButteryUIElement::Input(ButteryUIInput {
+                                current_value: self.secondary_text.clone(),
+                                on_changed: |value, game| {
+                                    game.secondary_text = value;
+                                },
+                                ..Default::default()
+                            }),
+                        ],
+                        centered: false,
+                        size: None,
+                    }),
+                ],
+                centered: true,
+                size: None,
+            }),
+            ..Default::default()
+        });
+
+        model
     }
 }
 
@@ -111,6 +248,8 @@ impl ButteryGame for ButteryExample {
 
         if self.open_menu {
             state.renderer.update_ui_model(Some(self.build_ui_model()));
+        } else {
+            state.renderer.update_ui_model(Some(self.build_hud_model()));
         }
     }
 
